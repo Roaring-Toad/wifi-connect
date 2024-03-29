6787b0b6a727979b5d42ab674bea06315a59a2ee
Paul Nettle <nettle.paul@gmail.com>
2024-03-29 10:54:34 -0500

    Replaced the startup script with a much better script that keeps the device online. This new script originated from github.com/balena-labs-project/wifi-connect, but includes some additional improvements. Most notably, after the wifi access point is closed, it will immediately check for connectivity and re-start the access point if there isn't one. This avoids the CHECK_CONN_FREQ down-time between access point attempts.

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,33 +1,43 @@
-#!/usr/bin/env bash
+#!/bin/busybox sh
 
-export DBUS_SYSTEM_BUS_ADDRESS=unix:path=/host/run/dbus/system_bus_socket
-
-# Optional step - it takes couple of seconds (or longer) to establish a WiFi connection
-# sometimes. In this case, following checks will fail and wifi-connect
-# will be launched even if the device will be able to connect to a WiFi network.
-# If this is your case, you can wait for a while and then check for the connection.
-# sleep 15
-
-# Choose a condition for running WiFi Connect according to your use case:
-
-# 1. Is there a default gateway?
-# ip route | grep default
-
-# 2. Is there Internet connectivity?
-# nmcli -t g | grep full
-
-# 3. Is there Internet connectivity via a google ping?
-# wget --spider http://google.com 2>&1
-
-# 4. Is there an active WiFi connection?
-iwgetid -r
-
-if [ $? -eq 0 ]; then
-    printf 'Skipping WiFi Connect\n'
-else
-    printf 'Starting WiFi Connect\n'
-    ./wifi-connect
+if [[ ! -z $CHECK_CONN_FREQ ]] 
+    then
+        freq=$CHECK_CONN_FREQ
+    else
+        freq=120
 fi
 
-# Start your application here.
-sleep infinity
+
+sleep 5
+PORTAL_WAS_RUN=0
+
+while [[ true ]]; do
+    if [[ $VERBOSE != false ]]; then echo "Checking internet connectivity ..."; fi
+    wget --spider --no-check-certificate 1.1.1.1 > /dev/null 2>&1
+
+    if [ $? -eq 0 ]; then
+        if [[ $VERBOSE != false ]]; then
+            if [[ $PORTAL_WAS_RUN == 1 ]]; then
+                echo "Your device was successfully connected to the internet, will check again in $freq seconds.";
+            else
+                echo "Your device is already connected to the internet, will check again in $freq seconds.";
+            fi
+        fi
+        PORTAL_WAS_RUN=0
+    else
+        if [[ $VERBOSE != false ]]; then
+            echo "Your device is not connected to the internet, starting up access point '${PORTAL_SSID}'."
+        fi
+        DBUS_SYSTEM_BUS_ADDRESS=unix:path=/host/run/dbus/system_bus_socket /usr/src/app/wifi-connect -u /usr/src/app/ui
+        if [[ $VERBOSE != false ]]; then
+            echo "The access point is stopped, will check for internet immediately."
+        fi
+        PORTAL_WAS_RUN=1
+        continue
+    fi
+
+    sleep $freq
+done
+
+
+/bin/busybox sh /usr/bin/balena-idle
